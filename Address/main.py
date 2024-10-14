cf0eda66c63011fb0fc00d70c048ea1d1c0e457e
george-novos <george.chant@novosfiber.com>
2024-10-14 15:32:07 -0500

    Update main.py

--- Address/main.py
+++ Address/main.py
@@ -5,5 +5,5 @@ if __name__ == "__main__":
     print("Starting..")
     address_df = pd.read_excel('master_address_list.xlsx')
     processed_addresses_df = split_street_direction_suffix(address_df)
-    processed_addresses_df.to_excel('processed_addresses.xlsx', index=False)
+    processed_addresses_df.to_csv('processed_addresses.csv', index=False)
     print("Done")
